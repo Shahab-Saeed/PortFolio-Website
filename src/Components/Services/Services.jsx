f9893e68999488b1ed53d206f54d4456e609ae76
Shahab-Saeed <shahabsaeed888@gmail.com>
2025-02-05 15:58:16 +0500

    a

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -15,14 +15,14 @@ const Services = () => {
                
                 
             </div>
-            <div className="service-box">
+           <div className="service-box">
                 <div className="service-info">
                     <h4>front End</h4>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam at cupiditate eum rem inventore in, asperiores vitae repellat nihil beatae, dicta quia possimus iste. Nihil repellat quae ducimus laudantium delectus impedit? Reprehenderit.</p>
                 </div>
                
                 
-            </div>
+            </div> 
            
         </div>
       </section>
